bdf4bc974631901a9df601b7304b7edb3f1901a3
José <andradejoseantonio0@gmail.com>
2022-10-07 15:58:22 -0300

    alterações em UserController - ainda não funcional

--- src/main/java/br/com/zeenance/controllers/UserController.java
+++ src/main/java/br/com/zeenance/controllers/UserController.java
@@ -1,15 +1,56 @@
 package br.com.zeenance.controllers;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javawithspring.data.vo.v1.PersonVO;
+import com.javawithspring.services.PersonServices;
+
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/person")
 public class UserController {
+		
+	@Autowired
+	private UserServices service;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
-		return "hey";
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<User> findAll() {
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public User findById(@PathVariable(value = "id") Long id) throws Exception {
+		return service.findById(id);
+	}
+	
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public User create(@RequestBody User user) {
+		return service.create(user);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public User update(@RequestBody User user){
+		return service.update(user);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
