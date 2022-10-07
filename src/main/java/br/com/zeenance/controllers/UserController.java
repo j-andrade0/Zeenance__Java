f97df784be732039a6a7e0d09898c82546447a97
José <andradejoseantonio0@gmail.com>
2022-10-07 16:37:40 -0300

    correção de rota em UserController

--- src/main/java/br/com/zeenance/controllers/UserController.java
+++ src/main/java/br/com/zeenance/controllers/UserController.java
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javawithspring.data.vo.v1.PersonVO;
-import com.javawithspring.services.PersonServices;
+import br.com.zeenance.models.entities.User;
+import br.com.zeenance.services.UserServices;
 
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/user")
 public class UserController {
 		
 	@Autowired
