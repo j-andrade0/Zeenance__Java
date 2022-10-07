a0ed759a59606ad90333b3159febd804e1106297
José <andradejoseantonio0@gmail.com>
2022-10-07 16:25:23 -0300

    alteração em repository, ao invés de extender CrudRepository, aogra extende JpaRepository

--- src/main/java/br/com/zeenance/models/repositories/UserRepository.java
+++ src/main/java/br/com/zeenance/models/repositories/UserRepository.java
@@ -1,9 +1,9 @@
 package br.com.zeenance.models.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.zeenance.models.entities.User;
 
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
